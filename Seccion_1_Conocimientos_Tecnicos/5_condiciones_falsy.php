ab480cb547a2495f4faf81e34c5edf9583b7b05b
cesar.bustos <cesar.bustos@hseqsisas.com>
2025-10-30 00:22:02 -0500

    se agrega respuesta del ejericio 5 seccion 1

---
+++ Seccion_1_Conocimientos_Tecnicos/5_condiciones_falsy.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ ¿Qué es una condición falsy? Da tres ejemplos en PHP y JavaScript.
+
+
+Una condición *falsy* es un valor que al evaluarse de forma boleana se considera false , es util para hacer validaciones  y detectar valores no validos
+
+Ejemplos
+
+Javascript
+
+if (0) {
+    console.log("Falsy");
+}
+
+if ("") {
+    console.log("Falsy");
+}
+}
+
+if (null) {
+    console.log("Falsy");
+}
+
+
+PHP
+
+if (0) {
+    echo "Falsy";
+}
+
+if ("") {
+    echo "Falsy";
+}
+
+if (null) {
+    echo "Falsy";
+}
+
+*/
+
+?>
