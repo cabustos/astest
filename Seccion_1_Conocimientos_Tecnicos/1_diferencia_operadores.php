9a0676934a755e9df9c1974f7c4d87154f40cec3
cesar.bustos <cesar.bustos@hseqsisas.com>
2025-10-29 23:48:07 -0500

    ajuste de seccion 1 pregunta 1

---
+++ Seccion_1_Conocimientos_Tecnicos/1_diferencia_operadores.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Diferencia entre == y === en PHP:
+
+== Compara solo el valor, permite conversiones de tipo automaticas pero puede no ser tan estricto, y en algunas casos generar respuestas inesperadas.
+=== Este operador compara el valor como el tipo de dato, siendo mas estricto y seguro evitando errores inesperados en las comparaciones
+
+*/
+
+?>
