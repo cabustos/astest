85f40d0ac36197a40ec32d991339899359c2785b
cesar.bustos <cesar.bustos@hseqsisas.com>
2025-10-30 00:07:46 -0500

    se agrega respuesta de la pregunta 3 seccion 1

---
+++ Seccion_1_Conocimientos_Tecnicos/3_on_vs_click.js
@@ -0,0 +1,12 @@
+
+/*
+¿Qué hace .on('click', ...) comparado con .click() en jQuery?
+
+.click() permito a un objeto existente asignar un evento click 
+
+mientras que
+
+.on('click', ...) permite de forma dinamica asignar evento click a elementos que se crean despues en el dom y tambien a elementos ya existentes
+
+
+*/
